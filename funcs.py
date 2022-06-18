dd6fb9d7845cedb59830a0fc4db014f005a8169a
sfome <semenilichevsky@gmail.com>
2022-06-19 01:21:36 +0300

    Update: add --gen funcs

--- funcs.py
+++ funcs.py
@@ -1,12 +1,16 @@
-from shutil import move
 from assets.asset import serv_name, serv_disc, serv_usr, serv_dir, serv_exec, serv_restart, serv_gen, serv_move, serv_act
+from os import system as sh
 
 
 def gen_service() -> None:
 
+    def yn() -> None:
+        print(" 1) \033[32mYes\033[37m")
+        print(" 0) \033[31mNo\033[37m")
+
     print(serv_name)
-    service_name: str = input(":")
     print(" example: cpupower")
+    service_name: str = input(":")
     print()
 
     print(serv_disc)
@@ -14,10 +18,12 @@ def gen_service() -> None:
     print()
 
     print(serv_usr)
+    print(" example: root")
     service_usr: str = input(":")
     print()
 
     print(serv_dir)
+    print(" example: /")
     service_dir: str = input(":")
     print()
 
@@ -26,8 +32,19 @@ def gen_service() -> None:
     print()
 
     print(serv_restart)
-    service_restart: str = input(":")
+    yn()
+    service_restart: int = int(input(":"))
     print()
+
+    if service_restart:
+        serv: str = f"[Unit]\nDescription={service_disc}\n\n[Service]\nUser={service_usr}\nWorkingDirectory={service_dir}\nExecStart={service_exec}\nRestart=always\n\n[Install]\nWantedBy=multi-user.target"
+        with open(f"{service_name}.service", "w") as f:
+            f.write(serv)
+
+    else:
+        serv_null: str = f"[Unit]\nDescription={service_disc}\n\n[Service]\nUser={service_usr}\nWorkingDirectory={service_dir}\nExecStart={service_exec}\n\n[Install]\nWantedBy=multi-user.target"
+        with open(f"{service_name}.service", "w") as f:
+            f.write(serv_null)
 
     print()
     print(serv_gen)
@@ -35,12 +52,22 @@ def gen_service() -> None:
 
     print()
     print(serv_move)
-    print(" 1) \033[32mYes\033[37m")
-    print(" 0) \033[31mNo\033[37m")
+    yn()
     service_move: int = int(input(":"))
 
-    print()
-    print(serv_act)
-    print(" 1) \033[32mYes\033[37m")
-    print(" 0) \033[31mNo\033[37m")
-    service_act: int = int(input(":"))
+    if service_move:
+        sh(f"sudo mv {service_name}.service /etc/systemd/system/")
+
+        print()
+        print(serv_act)
+        yn()
+        service_act: int = int(input(":"))
+
+        if service_act:
+            sh(f"sudo systemctl enable --now {service_name}")
+
+        else:
+            pass
+
+    else:
+        pass
--- uxptl.py
+++ uxptl.py
@@ -5,15 +5,19 @@ from flags import arg_pars
 from funcs import gen_service
 
 print(greeting)
+
 args: int = arg_pars(argv)
 
 if args == 1:
     gen_service()
 
+elif args == 2:
+    print("-m", "--move")
+
 elif args == 3:
     print("-s", "--activate")
 
-elif args == 3:
+elif args == 4:
     print("-v", "--version")
 
 else:
