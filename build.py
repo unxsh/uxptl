0d14308e31ac7153f6a0596b12660d12c5ff91fc
sfome <semenilichevsky@gmail.com>
2022-06-19 05:51:55 +0300

    Update: add build system

---
+++ build.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from os import system as sh
+from os import mkdir
+from shutil import rmtree
+from os import chdir
+from sys import argv
+
+
+MAIN: str = "uxptl.py"
+
+PYFLAGS: str = """ \
+    -OO \
+"""
+
+CFLAGS: str = """ \
+    --warn-implicit-exceptions \
+    --warn-unusual-code \
+    --follow-imports \
+    --python-flag=-OO \
+    --disable-ccache \
+    --lto=yes \
+"""
+
+BINDIR: str = "/usr/local/bin"
+
+
+BUILD = f"python {PYFLAGS} -m nuitka {CFLAGS} -o uxptl ../{MAIN}"
+
+
+if len(argv) == 1:
+    print("incorrect flags")
+
+elif argv[1] == "install":
+    try:
+        mkdir("build/")
+
+    except FileExistsError:
+        rmtree("build/")
+        mkdir("build/")
+        chdir("build/")
+
+    sh(BUILD)
+    sh(f"sudo mv uxptl {BINDIR}")
+
+elif argv[1] == "build":
+    try:
+        mkdir("build/")
+
+    except FileExistsError:
+        rmtree("build/")
+        mkdir("build/")
+        chdir("build/")
+
+    sh(BUILD)
+
+else:
+    print("incorrect flags")
