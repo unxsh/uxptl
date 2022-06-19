9cf205b1e204445502bf34b1f4334e35b4a1edbb
sfome <semenilichevsky@gmail.com>
2022-06-20 01:50:16 +0300

    Update: add OFILE option

--- build.py
+++ build.py
@@ -9,6 +9,8 @@ from sys import argv
 
 MAIN: str = "uxptl.py"
 
+OFILE: str = "uxptl"
+
 PYFLAGS: str = """ \
     -OO \
 """
@@ -25,7 +27,7 @@ CFLAGS: str = """ \
 BINDIR: str = "/usr/local/bin"
 
 
-BUILD: str = f"python {PYFLAGS} -m nuitka {CFLAGS} -o uxptl ../{MAIN}"
+BUILD: str = f"python {PYFLAGS} -m nuitka {CFLAGS} -o {OFILE} ../{MAIN}"
 
 
 if len(argv) == 1:
